ecdd23ac4a4e38858f4ad933c2464017595ecde0
Electronic Mango <78230210+Electronic-Mango@users.noreply.github.com>
2022-11-04 21:40:43 +0100

    Tweak history module variable names

--- src/history.py
+++ src/history.py
@@ -2,11 +2,11 @@ HISTORY_FILE_PATH = "history"
 
 
 def get_latest_id() -> int:
-    with open(HISTORY_FILE_PATH) as histfile:
-        previous_msg_id = histfile.read().strip()
+    with open(HISTORY_FILE_PATH) as history_file:
+        previous_msg_id = history_file.read().strip()
     return int(previous_msg_id) if previous_msg_id.isnumeric() else 0
 
 
 def store_latest_id(id: int) -> None:
-    with open(HISTORY_FILE_PATH, "w") as histfile:
-        histfile.write(str(id))
+    with open(HISTORY_FILE_PATH, "w") as history_file:
+        history_file.write(str(id))
